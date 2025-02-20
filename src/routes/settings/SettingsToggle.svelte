4a4ace3883ac372b7bd88c4fd09319056413e1bd
Osnott <hi@osnott.xyz>
2025-02-20 13:45:00 -0700

    feat: add toggle switcher to `SettingsToggle.svelte`
    implementation taken from cobalt @ https://github.com/imputnet/cobalt

--- src/routes/settings/SettingsToggle.svelte
+++ src/routes/settings/SettingsToggle.svelte
@@ -9,6 +9,8 @@
   import settings, { updateSetting } from "$lib/state/settings"
   import type { Settings } from "$lib/types/settings"
 
+  import Toggle from "./Toggle.svelte"
+
   export let settingId: Id
   export let settingContext: Context
 
@@ -18,7 +20,7 @@
   $: isEnabled = !!setting
 </script>
 
-<div>
+<div class="toggle-parent">
   <button
     class="toggle-container"
     role="switch"
@@ -31,5 +33,29 @@
     })}
   >
     <h4 class="toggle-title">{title}</h4>
+    <Toggle enabled={isEnabled} />
   </button>
 </div>
+
+<style>
+.toggle-parent {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow: hidden;
+  transition: opacity 0.2s;
+}
+
+.toggle-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  justify-content: space-between;
+  text-align: start;
+  transform: none;
+  padding: 8px;
+  overflow: hidden;
+}
+</style>
---
+++ src/routes/settings/Toggle.svelte
@@ -0,0 +1,53 @@
+<!-- implementation taken from cobalt @ https://github.com/imputnet/cobalt -->
+<script lang="ts">
+	export let enabled: boolean;
+</script>
+
+<div class="toggle" class:enabled>
+	<div class="toggle-switcher"></div>
+</div>
+
+<style>
+	.toggle {
+		--base-size: 22px;
+		--ratio-factor: 0.9;
+		--enabled-pos: calc(82% * var(--ratio-factor));
+
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		min-width: calc(var(--base-size) * (1 + var(--ratio-factor)));
+		padding: 2px;
+		aspect-ratio: 2/1;
+		border-radius: 100px;
+		background: var(--color-surface);
+		transition: background 0.2s;
+	}
+
+	.toggle:dir(rtl) {
+		--enabled-pos: calc(-82% * var(--ratio-factor));
+	}
+
+	.toggle-switcher {
+		height: var(--base-size);
+		width: var(--base-size);
+		background: var(--color-muted);
+		border-radius: 100px;
+		transform: translateX(0%);
+		transition:
+			transform 0.2s,
+			width 0.2s;
+	}
+
+	.toggle.enabled {
+		background: var(--color-theme-2);
+	}
+
+	.toggle.enabled .toggle-switcher {
+		transform: translateX(var(--enabled-pos));
+	}
+
+	:global(.toggle-container:active .toggle:not(.enabled) .toggle-switcher) {
+		width: calc(var(--base-size) * 1.15);
+	}
+</style>
